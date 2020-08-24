2aee4bc551a487487c26d13858178442fa71256d
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-24 11:52:59 -0500

    Create Friendship model

---
+++ db/migrate/20200824165157_create_friendships.rb
@@ -0,0 +1,11 @@
+class CreateFriendships < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friendships do |t|
+      t.references :user
+      t.references :friend
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
