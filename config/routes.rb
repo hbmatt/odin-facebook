d2b119355e673c21f489e31915193cdc536e654c
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-08-26 14:22:12 -0500

    Add Omniauth Facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
 
   resources :posts do
     resources :comments, except: [:index, :show, :new]
